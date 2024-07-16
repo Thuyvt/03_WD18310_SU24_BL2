5b81074e2d211e9f565a1ab7d7f2352bd84b5994
thuyvt <thuyvt1005@gmail.com>
2024-07-16 16:06:00 +0700

    day 8

---
+++ resources/views/books/create.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.master')
+
+@section('title')
+@endsection
+
+@section('content')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use App\Http\Controllers\NhacsiController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SanPhamController;
@@ -60,3 +61,6 @@ Route::get('/nhacsi/{id}/edit', [NhacsiController::class, 'edit'])->name('nhacsi
 Route::post('/nhacsi/store', [NhacsiController::class, 'store'])->name('nhacsi.store');
 Route::put('/nhacsi/{id}/update', [NhacsiController::class, 'update'])->name('nhacsi.update');
 Route::delete('/nhacsi/{id}/destroy', [NhacsiController::class, 'destroy'])->name('nhacsi.destroy');
+
+// Route resource
+Route::resource('books', BookController::class);
