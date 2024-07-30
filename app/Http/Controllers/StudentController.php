ea78cf9dd5ae4c3a6a0995dbc0ece240bbe861f0
thuyvt <thuyvt1005@gmail.com>
2024-07-30 13:21:40 +0700

    day 13

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Resources\StudentResource;
 use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 use Mockery\Exception;
 
 class StudentController extends Controller
@@ -49,7 +51,33 @@ class StudentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validate
+        $validator = Validator::make($request->all(), [
+            'ten' => ['required', 'string', 'max:50'],
+            'email' => ['required', 'email']
+        ]);
+        if ($validator->fails()) {
+            $res = [
+                'status' => false,
+                'message' => 'Lỗi kiểm tra dữ liệu',
+                'data' => $validator->errors()
+            ];
+            return response()->json($res, 400);
+        }
+        // Lưu ý dùng $request->all() trong trường hợp body gửi lên có key == tên trường trong csdl
+        // Student::query()->create($request->all());
+        $student = Student::query()->create([
+            'name' => $request->ten,
+            'email' => $request->email,
+            'major_id' => $request->maChuyenNganh,
+            'dob' => $request->ngaySinh,
+        ]);
+        $res = [
+              'status' => true,
+              'message' => 'Tạo mới thành công',
+              'data' => new StudentResource($student)
+        ];
+        return response()->json($res, 201);
     }
 
     /**
@@ -57,7 +85,22 @@ class StudentController extends Controller
      */
     public function show(string $id)
     {
-        //
+//        dd($id);
+        $student = Student::query()->where('id', $id)->first();
+        if (!$student) {
+            $res = [
+                'status' => false,
+                'message' => 'Không tìm thấy sinh viên'
+            ];
+            return response()->json($res, 404);
+        }
+        $res = [
+            'status' => true,
+            'message' => 'Tìm thấy sinh viên',
+            'data' => new StudentResource($student)
+        ];
+
+        return response()->json($res, 200);
     }
 
     /**
@@ -73,7 +116,47 @@ class StudentController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        // Validate
+        $validator = Validator::make($request->all(), [
+            'ten' => ['required', 'string', 'max:50'],
+            'email' => ['required', 'email']
+        ]);
+        if ($validator->fails()) {
+            $res = [
+                'status' => false,
+                'message' => 'Lỗi kiểm tra dữ liệu',
+                'data' => $validator->errors()
+            ];
+            return response()->json($res, 400);
+        }
+        // Tìm đối tượng
+        $student = Student::query()->where('id', $id)->first();
+        if (!$student) {
+            $res = [
+                'status' => false,
+                'message' => 'Không tìm thấy sinh viên'
+            ];
+            return response()->json($res, 404);
+        }
+        // cập nhật thông tin
+        // C1:
+        $student->update([
+            'name' => $request->ten,
+            'email' => $request->email,
+            'major_id' => $request->maChuyenNganh,
+            'dob' => $request->ngaySinh,
+        ]);
+//        // C2:
+//        $student->name = $request->ten;
+//        $student->email = $request->email;
+//        // ..
+//        $student->save();
+        $res = [
+            'status' => true,
+            'message' => 'Cập nhật thành công',
+            'data' => new StudentResource($student)
+        ];
+        return response()->json($res, 200);
     }
 
     /**
@@ -81,6 +164,23 @@ class StudentController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+//        DB::beginTransaction();
+        $student = Student::query()->where('id', $id)->first();
+        if (!$student) {
+            $res = [
+                'status' => false,
+                'message' => 'Không tìm thấy sinh viên'
+            ];
+            return response()->json($res, 404);
+        }
+        // xóa student
+        $student->delete();
+
+        $res = [
+            'status' => true,
+            'message' => 'Xóa thành công'
+        ];
+        return response()->json($res, 200);
+//        DB::commit();
     }
 }
